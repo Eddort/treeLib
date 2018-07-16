65aa02d46c99dd8e2a573f695094c9362eb7896e
Eddort <webisforce@gmail.com>
2018-07-16 20:08:54 +0300

    some add

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('!s!!ssss!')
+class User {
+	private _name: string = "ssss";
+
+	public get name(): string {
+		return this._name;
+	}
+
+	public set name(n: string) {
+		this._name = n;
+	}
+}
+let tom: User = new User();
+console.log(tom._name, tom.name);
